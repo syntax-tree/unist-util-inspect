271ed3f55643fbc741ed22a0a176b12b514a93df
Titus Wormer <tituswormer@gmail.com>
2017-11-18 10:20:10 -0500

    Update dev-dependencies

--- test.js
+++ test.js
@@ -2,10 +2,9 @@
 
 var test = require('tape');
 var chalk = require('chalk');
+var strip = require('strip-ansi');
 var retext = require('retext');
 var inspect = require('./');
-
-var strip = chalk.stripColor;
 
 var paragraph = 'Some simple text. Other “sentence”.';
 
